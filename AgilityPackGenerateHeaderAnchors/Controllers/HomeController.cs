603d6a658f0a348dc35f12e97f5f0b2fa0768bdd
Niels Swimberghe <nielsswimberghe@gmail.com>
2019-09-14 22:07:19 -0400

    remove unnecessary code and assets

--- AgilityPackGenerateHeaderAnchors/Controllers/HomeController.cs
+++ AgilityPackGenerateHeaderAnchors/Controllers/HomeController.cs
@@ -1,9 +1,5 @@
 ﻿using HtmlAgilityPack;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
-using System.Web;
 using System.Web.Mvc;
 
 namespace AgilityPackGenerateHeaderAnchors.Controllers
--- AgilityPackGenerateHeaderAnchors/Global.asax.cs
+++ AgilityPackGenerateHeaderAnchors/Global.asax.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace AgilityPackGenerateHeaderAnchors
@@ -12,10 +7,8 @@ namespace AgilityPackGenerateHeaderAnchors
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
